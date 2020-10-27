1547a72c8b0149207ba6b3fe906c252b8111aae9
Mahmoud A. ATALLAH <x.up@live.com>
2020-10-27 19:35:39 +0300

    Update WVDCustomURLRedirection.cs

--- Azure/WindowsVirtualDesktop/WVDCustomURLRedirection/WVDCustomURLRedirection.cs
+++ Azure/WindowsVirtualDesktop/WVDCustomURLRedirection/WVDCustomURLRedirection.cs
@@ -7,7 +7,7 @@ string OriginUrl = req.Headers.GetValues ("DISGUISED-HOST").FirstOrDefault ();
 log.Info ("RequestURI org: " + OriginUrl);
 //create response
 var response = req.CreateResponse (HttpStatusCode.MovedPermanently);
-if (OriginUrl.Contains ("wvd.haitex.it")) {
+if (OriginUrl.Contains ("wvd.3tallah.com")) {
 response.Headers.Location = new Uri ("https://rdweb.wvd.microsoft.com/arm/webclient");
 } else {
 log.Info ("error RequestURI org: " + OriginUrl);
